2ca9817404ecd844c062ae3453f2eed505b9e31d
Nejat <124082301+tripnly@users.noreply.github.com>
2025-09-12 18:54:29 +0100

    Refactor middleware for security and CORS settings

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,42 @@
+// config/middlewares.js
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+
+  // Hardened CSP so images/media and API requests work from your site
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          // Let Strapi send resources to http(s) without forcing upgrade
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+
+  // CORS: allow your SPA in dev and prod
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:5173',
+        'https://lisboacitypass.tripnly.com',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+      keepHeaderOnError: true,
+    },
+  },
+
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
