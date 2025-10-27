fde45d0f05d6e7948d11d813446362e7735d96cf
Nejat <nejat@nuvene.com>
2025-10-27 23:15:15 +0000

    feat: enable @amicaldo/strapi-google-maps plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+// config/plugins.js
+module.exports = ({ env }) => ({
+  'google-maps': {
+    enabled: true,
+    config: {
+      apiKey: env('GOOGLE_MAPS_API_KEY'), // set this in Cloud env vars
+    },
+  },
+});
